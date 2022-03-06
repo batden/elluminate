a20b1442e05a14ee3589d077530f55622c43d1dd
Philippe Jean Guillaumie <batden@sfr.fr>
2022-03-06 19:21:30 +0100

    ddcutil

--- elluminate.sh
+++ elluminate.sh
@@ -608,8 +608,7 @@ set_p_src() {
 
 get_preq() {
   ESRC=$(cat $HOME/.cache/ebuilds/storepath)
-
-cd $DLDIR
+  cd $DLDIR
   wget -c https://github.com/rockowitz/ddcutil/archive/refs/tags/v$DDTL.tar.gz
   tar xzvf v$DDTL.tar.gz -C $ESRC
   cd $ESRC/ddcutil-$DDTL

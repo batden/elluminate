e7b5f78d0d13279e058a60f71c66feffe0b463bb
Philippe Jean Guillaumie <batden@sfr.fr>
2022-04-08 07:24:25 +0200

    Fix typo

--- elluminate.sh
+++ elluminate.sh
@@ -23,7 +23,7 @@
 # Once installed, you can update your shiny new Enlightenment desktop whenever you want to.
 # However, because software gains entropy over time (performance regression, unexpected
 # behavior... and this is especially true when dealing directly with source code),
-# we highly recommend doing a complete uninstallation and reinstallion of your
+# we highly recommend doing a complete uninstallation and reinstallation of your
 # Enlightenment desktop every three weeks or so for an optimal user experience.
 
 # ELLUMINATE is written and maintained by batden@sfr.fr and carlasensa@sfr.fr,

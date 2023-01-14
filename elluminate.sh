5895ff833160003cfd24ed2eaed3b77255cfc207
Philippe Jean Guillaumie <similar@orange.fr>
2023-01-14 17:38:14 +0100

    Update elluminate.sh

--- elluminate.sh
+++ elluminate.sh
@@ -29,7 +29,7 @@
 # Enlightenment desktop every three weeks or so for an optimal user experience.
 
 # ELLUMINATE is written and maintained by batden@sfr.fr and carlasensa@sfr.fr,
-# feel free to use this script as you see fit.
+# this work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
 
 # Please consider starring our repositories to show your support.
 # Cheers!

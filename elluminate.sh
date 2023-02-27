e6377025a017ed4c069eafdcc4c94edfa6f4b2ec
Philippe Jean Guillaumie <batden@sfr.fr>
2023-02-27 08:04:30 +0100

    Minor correction

--- elluminate.sh
+++ elluminate.sh
@@ -3,7 +3,7 @@
 # This Bash script allows you to easily and safely install Enlightenment, along with
 # other EFL-based applications, on your K/X/Ubuntu LTS desktop system.
 
-# Supported Ubuntu version: Jammy Jellyfish.
+# Supported Ubuntu distribution: Jammy Jellyfish.
 
 # ELLUMINATE takes care of downloading, configuring and building everything you
 # need to enjoy the very latest version of the Enlightenment environment

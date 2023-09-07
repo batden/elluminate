baf89157bf70cb68b3270199359d776e2b727dbf
Philippe Jean Guillaumie <batden@sfr.fr>
2023-09-07 17:52:00 +0200

    Minor corrections

--- elluminate.sh
+++ elluminate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This Bash script allows you to easily and safely install Enlightenment, along with
+# This script allows you to easily and safely install Enlightenment, along with
 # other applications based on the Enlightenment Foundation Libraries (EFL),
 # in your K/X/Ubuntu LTS desktop system.
 
@@ -228,7 +228,7 @@ elap_stop() {
 #
 # To restore a backup, use the same command that was executed but with
 # the source and destination reversed:
-# e.g. cp -aR /home/riley/Documents/ebackups/E_1684737862 .e* /home/riley/
+# e.g. cp -aR /home/riley/Documents/ebackups/E_1694099880 .e* /home/riley/
 # (Then press Ctrl+Alt+End to restart Enlightenment if you are currently logged into.)
 #
 e_bkp() {

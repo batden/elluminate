253636c9a43660c4aa2dea438eceefa7b4c9cfa7
Philippe Jean Guillaumie <batden@sfr.fr>
2024-01-17 08:48:33 +0100

    Refactor cnt_dir()

--- elluminate.sh
+++ elluminate.sh
@@ -191,36 +191,35 @@ bin_deps() {
 # Check source dependencies.
 cnt_dir() {
   COUNT=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l)
+
+  if [ ! -d efl ] || [ ! -d enlightenment ]; then
+    printf "\n$BLDR%s %s\n" "FAILED TO DOWNLOAD MAIN COMPONENT."
+    printf "$BLDR%s $OFF%s\n\n" "SCRIPT ABORTED."
+    beep_exit
+    exit 1
+    # You can try downloading the missing file(s) manually (see CLONEFL or CLONE26), then relaunch
+    # the script and select option 1 again; or relaunch the script at a later time.
+    # In both cases, be sure to enter the same path for the Enlightenment source
+    # folders as you previously used.
+  fi
+
   case $COUNT in
   14)
-    printf "$BDG%s $OFF%s\n\n" "All programs have been downloaded successfully."
+    printf "$BLDG%s $OFF%s\n\n" "All programs have been downloaded successfully."
     beep_dl_complete
     sleep 2
     ;;
   0)
-    printf "\n$BDR%s %s\n" "OOPS! SOMETHING WENT WRONG."
-    printf "$BDR%s $OFF%s\n\n" "SCRIPT ABORTED."
+    printf "\n$BLDR%s %s\n" "OOPS! SOMETHING WENT WRONG."
+    printf "$BLDR%s $OFF%s\n\n" "SCRIPT ABORTED."
     beep_exit
     exit 1
     ;;
   *)
-    if [ ! -d efl ] || [ ! -d enlightenment ]; then
-      printf "\n$BDR%s %s\n" "FAILED TO DOWNLOAD MAIN COMPONENT."
-      printf "$BDR%s $OFF%s\n\n" "SCRIPT ABORTED."
-      beep_exit
-      exit 1
-      # You can try downloading the missing files manually (see CLONEFL or CLONE26), then relaunch
-      # the script and select option 1 again; or relaunch the script at a later time.
-      # In both cases, be sure to enter the same path for the Enlightenment source
-      # folders as you previously used.
-    fi
-
-    if [ $COUNT -lt 14 ]; then
-      printf "\n$BDY%s %s\n" "WARNING: ONLY $COUNT OF 14 PROGRAMS HAVE BEEN DOWNLOADED!"
-      printf "\n$BDY%s $OFF%s\n\n" "WAIT 12 SECONDS OR HIT CTRL+C TO EXIT NOW."
-      beep_attention
-      sleep 12
-    fi
+    printf "\n$BLDY%s %s\n" "WARNING: ONLY $COUNT OF 14 PROGRAMS HAVE BEEN DOWNLOADED!"
+    printf "\n$BLDY%s $OFF%s\n\n" "WAIT 12 SECONDS OR HIT CTRL+C TO EXIT NOW."
+    beep_attention
+    sleep 12
     ;;
   esac
 }

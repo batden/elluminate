3e4e1e921d7acc659d7e4235da0ef6ae0e290f31
Philippe Jean Guillaumie <batden@sfr.fr>
2024-04-25 09:16:17 +0200

    Minor corrections

--- elluminate.sh
+++ elluminate.sh
@@ -249,10 +249,10 @@ elap_stop() {
 #
 # To restore a backup, use the same commands that were executed but with
 # the source and destination reversed, similar to this:
-# cp -aR /home/riley/Documents/ebackups/E_713437727/.elementary/ /home/riley/
-# cp -aR /home/riley/Documents/ebackups/E_713437727/.e/ /home/riley/
-# cp -aR /home/riley/Documents/ebackups/ETERM_1713437727/terminology/config/ /home/riley/.config/terminology/
-# cp -aR /home/riley/Documents/ebackups/ETERM_1713437727/terminology/themes/ /home/riley/.config/terminology/
+# cp -aR /home/riley/Documents/ebackups/E_1714028828/.elementary/ /home/riley/
+# cp -aR /home/riley/Documents/ebackups/E_1714028828.e/ /home/riley/
+# cp -aR /home/riley/Documents/ebackups/ETERM_1714028828/terminology/config/ /home/riley/.config/terminology/
+# cp -aR /home/riley/Documents/ebackups/ETERM_1714028828/terminology/themes/ /home/riley/.config/terminology/
 # (Then press Ctrl+Alt+End to restart Enlightenment if you are currently logged into.)
 #
 e_bkp() {

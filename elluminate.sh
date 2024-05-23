7fea6ed5501ab94461be15722a33adfd96cf8560
Philippe Jean Guillaumie <batden@sfr.fr>
2024-05-23 09:44:14 +0200

    Enable workaround

--- elluminate.sh
+++ elluminate.sh
@@ -599,13 +599,13 @@ do_bsh_alias() {
     export LDFLAGS=-L/usr/local/lib
     export PKG_CONFIG_PATH=/usr/local/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig
 
-    # Optional keyring service workaround for Enlightenment.
-    # (You also need to autostart some additional services at startup for this to work:
-    # See the repository's wiki for more info.)
-    # Uncomment the following if you know what you are doing.
-    #if [ "$XDG_CURRENT_DESKTOP" == "Enlightenment" ]; then
-      #export SSH_AUTH_SOCK=/run/user/${UID}/keyring/ssh
-    #fi
+    # Keyring service workaround for Enlightenment.
+    # You also need to autostart some additional services at startup for this to work:
+    # See the repository's wiki for more info.
+    #
+    if [ "$XDG_CURRENT_DESKTOP" == "Enlightenment" ]; then
+      export SSH_AUTH_SOCK=/run/user/${UID}/keyring/ssh
+    fi
 EOF
 
   source $HOME/.bash_aliases

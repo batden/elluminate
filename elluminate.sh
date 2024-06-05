699118597db52baaa363d7740b3fa6abc39b08ed
Philippe Jean Guillaumie <batden@sfr.fr>
2024-06-05 17:39:24 +0200

    Check for pv command

--- elluminate.sh
+++ elluminate.sh
@@ -816,6 +816,12 @@ wld_go() {
   exit 0
 }
 
+chk_pv() {
+  if [ ! -x /usr/bin/pv ]; then
+    sudo apt install -y pv
+  fi
+}
+
 # Lo and behold (“bhd”)!
 #
 # Display the selection menu...
@@ -850,5 +856,6 @@ bhd() {
   fi
 }
 
+chk_pv
 lo
 bhd

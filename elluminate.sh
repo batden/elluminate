9b5e2bcc81c70a50b645a9066bf06d53cc3b1d36
Philippe Jean Guillaumie <batden@sfr.fr>
2025-04-02 17:37:07 +0200

    Minor corrections

--- elluminate.sh
+++ elluminate.sh
@@ -838,7 +838,7 @@ release_go() {
   rebuild_optim
 
   sudo mv -f /usr/local/share/xsessions/enlightenment.desktop \
-    /usr/share/xsessions/enlightenment.desktop
+    /usr/share/xsessions/enlightenment.desktop &>/dev/null
 
   if [ -f /usr/local/share/wayland-sessions/enlightenment-wayland.desktop ]; then
     sudo rm -rf /usr/local/share/wayland-sessions/enlightenment-wayland.desktop
@@ -871,7 +871,7 @@ wld_go() {
 
   sudo mkdir -p /usr/share/wayland-sessions
   sudo mv -f /usr/local/share/wayland-sessions/enlightenment-wayland.desktop \
-    /usr/share/wayland-sessions/enlightenment-wayland.desktop
+    /usr/share/wayland-sessions/enlightenment-wayland.desktop &>/dev/null
 
   beep_ok
 

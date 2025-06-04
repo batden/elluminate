bcddd00c61bb78bfb0d4f9ba9c844f57eea1598d
Philippe Jean Guillaumie <batden@sfr.fr>
2025-06-04 18:57:30 +0200

    Minor corrections

--- elluminate.sh
+++ elluminate.sh
@@ -9,7 +9,7 @@
 
 # ELLUMINATE.SH takes care of downloading, configuring, and building everything
 # you need to enjoy the very latest version of the Enlightenment environment
-# (DEB packages ——if they exist—— often lag far behind). Once installed,
+# (DEB packages——if they exist——often lag far behind). Once installed,
 # you can update your Enlightenment desktop whenever you like.
 
 # In order not to force our existing users to reinstall everything from scratch,
@@ -829,8 +829,8 @@ install_now() {
   beep_ok
 
   printf "\n\n$blue_bright%s %s" "INITIAL SETUP WIZARD TIPS:"
-  printf "\n$blue_bright%s %s" '“Enable update checking” —— You can disable this feature as it is not helpful for this type of installation.'
-  printf "\n$blue_bright%s $off%s\n\n" '“Network management support” —— Connman is not required. You can ignore the message that appears.'
+  printf "\n$blue_bright%s %s" '“Enable update checking” — You can disable this feature as it is not helpful for this type of installation.'
+  printf "\n$blue_bright%s $off%s\n\n" '“Network management support” — Connman is not required. You can ignore the message that appears.'
 
   # Note: Enlightenment adds three shortcut icons (namely home.desktop, root.desktop and tmp.desktop)
   # to your Desktop, you can safely delete them if it bothers you.

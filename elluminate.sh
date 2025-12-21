6c20c33ae8575bd6078e9f015891470755effe1f
Philippe Jean Guillaumie <batden@sfr.fr>
2025-12-21 12:00:55 +0100

    Minor changes

--- elluminate.sh
+++ elluminate.sh
@@ -328,10 +328,10 @@ e_bkp() {
   # or visit https://www.epochconverter.com/
   # To restore a backup, use the same commands that were run, but with the source
   # and destination reversed, similar to this, in a terminal:
-  # cp -aR /home/riley/Documents/ebackups/e_1747988712/.elementary/ /home/riley/
-  # cp -aR /home/riley/Documents/ebackups/e_1747988712/.e/ /home/riley/
-  # cp -aR /home/riley/Documents/ebackups/eterm_1747988712/terminology/config/ /home/riley/.config/terminology/
-  # cp -aR /home/riley/Documents/ebackups/eterm_1747988712/terminology/themes/ /home/riley/.config/terminology/
+  # cp -aR /home/riley/Documents/ebackups/e_1766314240/.elementary/ /home/riley/
+  # cp -aR /home/riley/Documents/ebackups/e_1766314240/.e/ /home/riley/
+  # cp -aR /home/riley/Documents/ebackups/eterm_1766314240/terminology/config/ /home/riley/.config/terminology/
+  # cp -aR /home/riley/Documents/ebackups/eterm_1766314240/terminology/themes/ /home/riley/.config/terminology/
   # Then close the terminal and press Ctrl+Alt+End to restart Enlightenment if you are logged in.
 }
 

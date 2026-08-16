2cb315f21c58feeabca7272319c496fbdde9d7d2
Philippe Jean Guillaumie <batden@sfr.fr>
2026-08-16 13:18:30 +0200

    Akk chk_mn()

--- elluminate.sh
+++ elluminate.sh
@@ -739,16 +739,20 @@ chk_sl() {
 
   if [ ! -x /usr/bin/meson ]; then
     printf "\n$bold%s $off%s\n\n" "Installing the recommended version of the meson package..."
+    sleep 1
+    sudo apt install -y python3-setuptools
     cd "$dldir"
     wget https://launchpadlibrarian.net/850294555/meson_$mson.deb
-    sudo apt install ./meson_$mson.deb
+    sudo dpkg -i meson_$mson.deb
     rm meson_$mson.deb
     cd "$HOME"
   elif [ "$installed_meson_version" != "1.10.1-1ubuntu2" ]; then
     printf "\n$bold%s $off%s\n\n" "Updating Meson to the recommended version..."
+    sleep 1
+    sudo apt install -y python3-setuptools
     cd "$dldir"
     wget https://launchpadlibrarian.net/850294555/meson_$mson.deb
-    sudo apt install ./meson_$mson.deb
+    sudo dpkg -i meson_$mson.deb
     rm meson_$mson.deb
     cd "$HOME"
   fi

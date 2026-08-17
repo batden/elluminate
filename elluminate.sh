2ee11b8ce1670bed25d79b2f13bae4bf9207e4dc
Philippe Jean Guillaumie <batden@sfr.fr>
2026-08-17 17:57:18 +0200

    Minor corrections

--- elluminate.sh
+++ elluminate.sh
@@ -233,11 +233,11 @@ menu_selec() {
 
   echo
   if [ "$is_einstl" == false ]; then
-    printf "1  $green_bright%s $off%s\n\n" "INSTALL the Enlightenment ecosystem now" | pv -qL 20
+    printf "1  $green_bright%s $off%s\n\n" "INSTALL the Enlightenment ecosystem" | pv -qL 20
     printf "2  $magenta_dim%s $off%s\n\n" "(Update and rebuild the ecosystem on Xorg)" | pv -qL 30
     printf "3  $orange_dim%s $off%s\n\n" "(Update and rebuild the ecosystem with Wayland support)" | pv -qL 30
   else
-    printf "1  $green_dim%s $off%s\n\n" "(Install the Enlightenment ecosystem now)" | pv -qL 30
+    printf "1  $green_dim%s $off%s\n\n" "(Install the Enlightenment ecosystem)" | pv -qL 30
     printf "2  $magenta_bright%s $off%s\n\n" "Update and rebuild the ecosystem on Xorg" | pv -qL 20
     printf "3  $orange_bright%s $off%s\n\n" "Update and rebuild the ecosystem with Wayland support" | pv -qL 24
   fi

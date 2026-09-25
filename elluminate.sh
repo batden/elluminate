4ccaa7870b682f06b5f74b685171e858149d3eca
Philippe Jean Guillaumie <batden@sfr.fr>
2026-09-25 20:35:25 +0200

    Minor change

--- elluminate.sh
+++ elluminate.sh
@@ -34,7 +34,7 @@
 # in memory of Aaron Swartz.
 # See https://creativecommons.org/licenses/by/4.0/
 
-# If you find our scripts useful, please consider starring our repositories. Thank you!
+# If you find our scripts useful, please star our repositories. Thank you!
 
 # --- Uncomment the following line if you want the script to output in C locale. ---
 #export LANG=C
